a0536c0d1c5f039b02a0cb8ed56d283be7c4b23e
Matias Gonzalez <matias92meg@gmail.com>
2024-01-24 20:46:07 -0300

    haaaaaaaaaaaaaaaaaaaa

--- src/models/cartManager.js
+++ src/models/cartManager.js
@@ -2,17 +2,16 @@ import { promises as fs } from "fs";
 import { fileURLToPath } from "url";
 import { join, dirname } from "path";
 import crypto from "crypto"
+import { ProductManager } from "./productManager.js";
 
-
+const productCart = new ProductManager('./products.json')
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
-
 export class CartManager {
     constructor() {
         this.path = join(__dirname, '../../carts.json')
         this.path2 = join(__dirname, '../../products.json')
     }
-
     async addCart() {
         const carts = JSON.parse(await fs.readFile(this.path), 'utf-8', null, 2)
         const existCart = carts.find(cart => cart.id === cart.cid)
@@ -23,29 +22,56 @@ export class CartManager {
             const newCart = { "id": carts.id, "products": [] }
             carts.push(newCart)
             await fs.writeFile(this.path, JSON.stringify(carts))
-            return true
+            return newCart
         }
     }
-
-
     async getCartById(cid) {
         const carts = JSON.parse(await fs.readFile(this.path), 'utf-8', null, 2)
+        console.log(carts)
         const cart = carts.find(cart => cart.id === cid)
         return cart
     }
 
-    async addProductToCart(cid, pid) {
-        const carts = JSON.parse(await fs.readFile(this.path))
-        const cart = carts.findIndex(cart => cart.id === cid)
-        const prods = JSON.parse(await fs.readFile(this.path2))
-        const prod = carts.findIndex(producto => producto.id === pid)
-        if (prod != -1) {
-            return carts[prod].quantity++
-        } else {
-            newCart = await this.addCart(cart)
-            cart.push(prod)
-            await fs.writeFile(this.path, JSON.stringify(prod))
-            return true
+    // async addProductToCart(cid, pid) {
+    //     const cart = getCartById(cid)
+    //     const prod = cart.findIndex(producto => producto.id === pid)
+    //     console.log (prod)
+    //     if (prod != -1) {
+    //         return cart[prod].quantity++
+    //     } else {
+    //         newCart = await (this.addCart(cart))
+    //         cart.push(prod)
+    //         await fs.writeFile(this.path, JSON.stringify(prod))
+    //         return true
+    //     }
+    // }
+    async getProducts() {
+        try {
+            const prods = await fs.readFile(this.path2, "utf-8");
+            return JSON.parse(prods);
+        } catch (error) {
+            console.error("No se leyeron los productos:", error);
+            return [];
         }
+    }
+    async addProductToCart(cid, pid, quantity) {
+        const cart = await this.getCartById(cid)
+        const prods = await productCart.getProductsById(pid)
+        console.log (cart)
+        console.log (prods)
+        if (!prods || !cart) {
+            return false
+        }
+        const existProd = cart.products.find(prod => prod.id === pord.pid)
+        if (existProd) {
+            existProd.quantity += (quantity);
+        } else {
+            cart.products.push({
+                id: prods.id,
+                quantity: (quantity)
+            })
+        }
+        await fs.writeFile(this.path, JSON.stringify(cart))
+        return true
     }
 }
--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -11,25 +11,38 @@ routerCart.post('/', async (req, res) => {
     } else
         res.status(200).send(cart)
 })
-routerCart.get('/:cid', async (req, res) => {
-    const { cid } = req.params
-    const cart = await cartManager.getCartById(cid)
+routerCart.post('/:cid/products/:pid', async (req, res) => {
+    console.log('Carrito producto')
+    const { cid,pid } = req.params
 
-    if (cart) {
-        res.status(200).send({cart})
-    } else {
-        res.status(404).send('No se puedo encontrar carrito con el ID ingresado')
-    }
-})
-routerCart.post('/:cid/products/:pid'), async (req, res) => {
-    const { cid } = req.params
-    const { pid } = req.params
-    const addToCart = await cartManager.addProductToCart(cid, pid)
+    const { quantity } = req.body
+    const addToCart = await cartManager.addProductToCart(cid, pid, quantity)
     if (addToCart) {
         res.status(201).send(`Se agregó correctamente el producto ${pid}`)
     } else {
         res.status(400).send(`Algo salió mal. No se pudo agregar el producto ${pid} al carrito${cid}`)
     }
+    // const { cid } = req.params
+    // const cart = await cartManager.getCartById(cid)
+
+    // if (cart) {
+    //     res.status(200).send({cart})
+    // } else {
+    //     res.status(404).send('No se puedo encontrar carrito con el ID ingresado')
+    // }
+})
+routerCart.post('/:cid/products/:pid'), async (req, res) => {
+    console.log('Carrito producto')
+    const { cid,pid } = req.params
+
+    const { quantity } = req.body
+    const addToCart = await cartManager.addProductToCart(cid, pid, quantity)
+    if (addToCart) {
+        res.status(201).send(`Se agregó correctamente el producto ${pid}`)
+    } else {
+        res.status(400).send(`Algo salió mal. No se pudo agregar el producto ${pid} al carrito${cid}`)
+    }
+    
 }
 
 
